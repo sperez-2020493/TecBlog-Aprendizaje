eb5aa2b94dac55f15b934f20b49d95992276ca82
sperez-2020493 <sperez-2020493@kinal.edu.gt>
2025-04-06 15:12:32 -0600

    Base del proyecto

---
+++ configs/server.js
@@ -0,0 +1,46 @@
+"use strict"
+
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import { dbConnection } from "./mongo.js"
+import apiLimiter from "../src/middlewares/validar-cant-peticiones.js"
+
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+    app.use(apiLimiter)
+}
+
+/*
+const routes = (app) =>{
+
+}
+    */
+
+const conectarDB = async () =>{
+    try{
+        await dbConnection()
+    }catch(err){
+        console.log(`Database connection failed: ${err}`)
+        process.exit(1)
+    }
+}
+
+export const initiServer = () => {
+    const app = express()
+    try{
+        middlewares(app)
+        conectarDB()
+       // routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
+    }catch(err){
+        console.log(`Server init failed: ${err}`)
+    }
+}
